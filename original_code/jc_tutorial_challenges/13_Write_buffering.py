c43725e19dd7b3e21862649f5e3b831f2df0532f
wernicka <awernicke@eabloaner-dc-01.myeab.com>
2019-02-21 18:17:16 -0500

    Update 13_Write_buffering.py
    struggling with csv.writer

--- original_code/jc_tutorial_challenges/13_Write_buffering.py
+++ original_code/jc_tutorial_challenges/13_Write_buffering.py
@@ -2,3 +2,22 @@
 # IMPORTANT: Import a CSV or text file and do something to each line.  Write the
 # new content to a second file, but only write ONCE. (Lesson: file writes are
 # expensive/slow!)
+
+f_names = ['5','Fashima', 'Felix', 'Fabio', 'Farah', 'Fay']
+new_data = ''
+
+import csv
+with open('names.csv', newline='\n') as file:
+    data = csv.reader(file, delimiter=',')
+    master_list = []
+    for row in data:
+        master_list.append(row)
+    for i in range(6):
+        master_list[i].append(f_names[i])
+        new_data += str(master_list[i]) + '\n'
+#print(new_data)
+
+with open('names_with_f.csv', 'w', newline='\n') as file:
+    writer = csv.writer(file)
+    for row in new_data:
+        writer.writerow(row)
