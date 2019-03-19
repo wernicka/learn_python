d1d03e2fc566178cdb7e24d24a8af4af24e843e2
wernicka <awernicke@eabloaner-dc-01.myeab.com>
2019-03-19 15:01:20 -0400

    updated exercise 19 based on Amey's feedback
    -consolidated if statements
    -consolidated 2 loops into 1

--- original_code/jc_tutorial_challenges/19_file_aggregation_1.py
+++ original_code/jc_tutorial_challenges/19_file_aggregation_1.py
@@ -10,19 +10,14 @@ master_list = []
 
 mypath = "/Users/awernicke/Documents/GitHub/learn_python/original_code/jc_tutorial_challenges/csv_files"
 
-paths_to_files = []
 files_and_directories = listdir(mypath)
 for file_and_directory_name in files_and_directories:
     path_to_file_or_dir = join(mypath, file_and_directory_name)
-    if isfile(path_to_file_or_dir):
-        if path_to_file_or_dir.endswith('.csv'):
-            paths_to_files.append(path_to_file_or_dir)
-
-for file in paths_to_files:
-    with open(file, newline='\n') as file:
-        data = csv.reader(file, delimiter=',')
-        for row in data:
-            master_list.append(row)
+    if isfile(path_to_file_or_dir) and path_to_file_or_dir.endswith('.csv'):
+        with open(path_to_file_or_dir, newline='\n') as file:
+            data = csv.reader(file, delimiter=',')
+            for row in data:
+                master_list.append(row)
 
 with open('master_file.csv', 'w', newline='') as file:
     writer = csv.writer(file)
