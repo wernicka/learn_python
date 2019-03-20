eb28cb61aa03dc4eff873cb4079d614d612195fe
wernicka <awernicke@eabloaner-dc-01.myeab.com>
2019-03-20 14:04:54 -0400

    Update 24_reverse_thesaurus.py
    solved exercise 24: dictionary value can be a list

--- original_code/jc_tutorial_challenges/24_reverse_thesaurus.py
+++ original_code/jc_tutorial_challenges/24_reverse_thesaurus.py
@@ -5,41 +5,16 @@
 
 import csv
 
-#mydict = {}
+mydict = {}
 
 with open('./csv_files/thesaurus.csv', newline='\n') as csvfile:
     data = csv.reader(csvfile, delimiter=',')
-    Y_values = []
-    X_values = []
-    Y_value_count = 0
     for row in data:
         #row type is list
-        if row[1] not in Y_values:
-            Y_values.append(row[1])
-        for Y_value in Y_values:
-            Y_value_count += 1
-            X_values.append(row[1],row[0])
+        mydict.setdefault(row[1],[])
+        #adds Y as a dictionary key if it's not already a key, with an empty list as the value
+        mydict[row[1]].append(row[0])
+        #append X to the value list for that Y's key
 
-print(Y_values)
-print(X_values)
-
-        # key = row[Y]
-        # val = row[X]
-        # mydict[key] = (val)
-
-        # file = open('inventory.csv')
-        # content = file.read()
-        # file.close()
-        #
-        # my_things = {}
-        # split_content = content.split("\n")
-        # # split_content type is list
-        # for line in split_content:
-        #     # line type is string
-        #     line = line.strip()
-        #     if line:
-        #         (key, val) = line.split(",")
-        #         my_things[key] = int(val)
-        #
-        # for key, val in my_things.items():
-        #     print(f"{key} : {val}")
+for key,value in mydict.items():
+    print(f"{key}: {len(value)}: {value}")
